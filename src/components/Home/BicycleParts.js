f3ad2decebd62082843dc1663f43a8a67e9a62a6
19khaled88 <69922782+19khaled88@users.noreply.github.com>
2022-05-22 02:03:36 +0600

    bicycle tools

---
+++ src/components/Home/BicycleParts.js
@@ -0,0 +1,127 @@
+import React from 'react'
+
+const BicycleParts = () => {
+  return (
+    <div className="flex flex-col">
+      <p className="text-3xl pt-16 pb-10">Bicycle Tools</p>
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 grid-cols-1 gap-4 px-5 container mx-auto">
+        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              Shoes!
+              <div class="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <div class="badge badge-outline">Fashion</div>
+              <div class="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              Shoes!
+              <div class="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <div class="badge badge-outline">Fashion</div>
+              <div class="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              Shoes!
+              <div class="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <div class="badge badge-outline">Fashion</div>
+              <div class="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              Shoes!
+              <div class="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <div class="badge badge-outline">Fashion</div>
+              <div class="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              Shoes!
+              <div class="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <div class="badge badge-outline">Fashion</div>
+              <div class="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+        <div class="card lg:max-w-lg bg-base-100 shadow-xl">
+          <figure>
+            <img
+              src="https://api.lorem.space/image/shoes?w=400&h=225"
+              alt="Shoes"
+            />
+          </figure>
+          <div class="card-body">
+            <h2 class="card-title">
+              Shoes!
+              <div class="badge badge-secondary">NEW</div>
+            </h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-end">
+              <div class="badge badge-outline">Fashion</div>
+              <div class="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default BicycleParts
